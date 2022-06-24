3748483c1c792c213ccd17ce1000bbaff885eece
apis8 <apis80@mail.ru>
2022-06-24 21:38:19 +0100

    первый комит 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class    Main {
     public static void main(String[] args) {
+        int[] dom = new int[] {1,2,3};
+        float[] f1 = new float[] {1.57f, 7.654f, 9.986f };
+        int[] x = new int[] {1,2,3,4,5};
 
         System.out.println("Hello world!");
         System.out.println("Hello world!");
